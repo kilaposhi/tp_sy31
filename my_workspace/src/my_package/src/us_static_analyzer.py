385b16eb54a741a2d5c803116b57cbb389f2f6e4
kilaposhi <kilapang@gmail.com>
2023-03-30 14:40:08 +0200

    la

--- my_workspace/src/my_package/src/us_static_analyzer.py
+++ my_workspace/src/my_package/src/us_static_analyzer.py
@@ -26,6 +26,10 @@ def load_bag():
 if __name__=="__main__":
     times, values = load_bag()
     print(times, values)
+    plt.hist(values)
+
+    medfilt(values)
+    plt.hist(values)
 
     # TODO: Analysis
     
