12f0a0357ccbd8472151944eb60bee306d5ea642
Nicolas Taupin <nicolas.taupin312@gmail.com>
2023-04-07 15:43:08 +0200

    je sais pas trop ce que je fais

--- my_workspace/src/my_package/src/moindre_carre.py
+++ my_workspace/src/my_package/src/moindre_carre.py
@@ -1,12 +1,22 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.signal import medfilt
+from scipy.stats import norm
 
-def moindre_carre(x,y):
+def moindre_carre(x, y):
 
+    #fontion qui calcule la droite des moindres carrés
     x_mean = np.mean(x)
     y_mean = np.mean(y)
     a_hat = np.sum((x-x_mean)*(y-y_mean)) / np.sum((x-x_mean)**2)
     b_hat = y_mean - a_hat*x_mean
+    
+    return a_hat, b_hat
+    """
+    
     print("Droite des moindres carrés (y = a*x+b) : a =", a_hat, "; b =", b_hat)
-    _, ax = plt.subplots(1, 1)
+    
+    ax = plt.subplot(1, 1)
     ax.scatter(x, y)
     ax.set_xlim((0, None))
     ax.set_ylim((0, None))
@@ -14,4 +24,4 @@ def moindre_carre(x,y):
     ax.set_ylabel("y")
     x_max = ax.get_xlim()[1]
     ax.plot([0, x_max], [b_hat, a_hat*x_max+b_hat], color='red')
-    plt.show()
+    plt.show()"""
