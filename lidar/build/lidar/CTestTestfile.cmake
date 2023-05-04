18ae53f2f48fc72e6d52b338a2a68411d24339ee
Kilaposhi <kilapang@gmail.com>
2023-05-04 17:28:45 +0200

    clean build/ and devel/

--- lidar/build/lidar/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ubuntu/SY31/tp_sy31/lidar/src/lidar
-# Build directory: /home/ubuntu/SY31/tp_sy31/lidar/build/lidar
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
