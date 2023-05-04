a1bcd606b681239efbd6ca0b2d3b069f122e81ec
Kilaposhi <kilapang.huichalaf@etu.utc.fr>
2023-05-04 09:55:23 +0200

    TP4 files

---
+++ clusterer.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+
+import numpy as np
+import rospy
+
+# Type of input and output messages
+from sensor_msgs.point_cloud2 import create_cloud, read_points
+from sensor_msgs.msg import PointCloud2, PointField
+
+PC2FIELDS = [PointField('x', 0, PointField.FLOAT32, 1),
+             PointField('y', 4, PointField.FLOAT32, 1),
+             PointField('z', 8, PointField.FLOAT32, 1),
+             PointField('c', 12, PointField.INT16, 1)
+]
+
+def callback(msg):
+    points = np.array(list(read_points(msg)))[:,:2]
+    groups = np.zeros(points.shape[0], dtype=int)
+
+    # ToDo: Determine k and D values
+    k = 
+    D = 
+
+    # ToDo: Clustering algorithm
+    for i in range(k, points.shape[0]):
+
+    clust_msg = create_cloud(msg.header, PC2FIELDS, [[points[i,0],points[i,1],0,c] for i,c in enumerate(groups)])
+    pub_clusters.publish(clust_msg)
+
+if __name__ == '__main__':
+    rospy.init_node('clusterer')
+    pub_clusters = rospy.Publisher('/lidar/clusters', PointCloud2, queue_size=10)
+    rospy.Subscriber('/lidar/points', PointCloud2, callback)
+    rospy.spin()
