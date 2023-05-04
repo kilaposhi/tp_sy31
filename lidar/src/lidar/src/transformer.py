428e2bca8d1a950c68e701d96e1590d2128201b1
Kilaposhi <kilapang@gmail.com>
2023-05-04 17:46:18 +0200

    refactor transformer.py

--- lidar/src/lidar/src/transformer.py
+++ lidar/src/lidar/src/transformer.py
@@ -1,36 +1,45 @@
 #!/usr/bin/env python3
 
+from __future__ import annotations
 import numpy as np
 import rospy
 
-# Type of input and output messages
 from sensor_msgs.point_cloud2 import create_cloud
 from sensor_msgs.msg import LaserScan, PointCloud2, PointField
 
-PC2FIELDS = [PointField('x', 0, PointField.FLOAT32, 1),
+
+MIN_DISTANCE = 0.1 #[m]
+
+cartesian_coord = (float,float)
+
+PointCloud2FIELDS = [PointField('x', 0, PointField.FLOAT32, 1),
              PointField('y', 4, PointField.FLOAT32, 1),
              PointField('z', 8, PointField.FLOAT32, 1),
              PointField('c', 12, PointField.INT16, 1)
 ]
 
+
+# LaserScan doc : http://docs.ros.org/en/melodic/api/sensor_msgs/html/msg/LaserScan.html
 def callback(msg : LaserScan):
-    coords = []
+    coordinates = []
 
+    def polar_To_cartesian_coord(radius : float, theta : float ) -> cartesian_coord:
+        x = radius * np.cos(theta)
+        y = radius * np.sin(theta)
+        return (x,y)
+
+    
     for i, theta in enumerate(np.arange(msg.angle_min, msg.angle_max, msg.angle_increment)):
-        # ToDo: Remove points too close
-        if msg.ranges[i] < 0.1 :
-            pass        # ToDo: Polar to Cartesian transformation
+        if msg.ranges[i] < MIN_DISTANCE:
+            pass        
+        point = polar_To_cartesian_coord(msg.ranges[i], theta)    
+        coordinates.append(point)
 
-        x = msg.ranges[i]*np.cos(theta)
-        y = msg.ranges[i]*np.sin(theta)
-        coords.append([x,y])
-
-    # Create a PointCloud2 message from coordinates
-    pc2 = create_cloud(msg.header, PC2FIELDS, [[x,y,0,0] for x,y in coords])
-    pub_pc2.publish(pc2)
+    pointCloud2 = create_cloud(msg.header, PointCloud2FIELDS, [[x,y,0,0] for x,y in coordinates])
+    publish_pointCloud2.publish(pointCloud2)
 
 if __name__ == '__main__':
     rospy.init_node('transformer')
-    pub_pc2 = rospy.Publisher('/lidar/points', PointCloud2, queue_size=10)
+    publish_pointCloud2 = rospy.Publisher('/lidar/points', PointCloud2, queue_size=10)
     rospy.Subscriber('/scan', LaserScan, callback)
     rospy.spin()
